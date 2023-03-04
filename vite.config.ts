f0112759f5d2033fd4377fedea5ad990e42b2fbe
Maycon Liborio <liborio.md@gmail.com>
2023-03-04 20:30:13 -0300

    :books: docs: Atualizando configurações do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import graphqlPlugin from 'vite-plugin-graphql';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  build: {
+    rollupOptions: {
+      external: ['graphql']
+    }
+  },
   plugins: [
+      graphqlPlugin,
     vue({
       template: { transformAssetUrls }
     }),
 
     quasar({
       sassVariables: 'src/styles/variables.scss'
-    })
-  ]
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+
+  optimizeDeps: {
+    include: [
+      'vue',
+      'vue-router',
+      '@vueuse/core',
+    ],
+  },
 })
