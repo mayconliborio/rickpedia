667b118c75d0c197071884480f9a2335d7ea0c9a
Maycon Liborio <liborio.md@gmail.com>
2023-03-05 00:53:47 -0300

    :sparkles: feature: Melhorias nos componentes de card

--- src/components/CardCharacter.vue
+++ src/components/CardCharacter.vue
@@ -32,11 +32,7 @@ img {
 .my-card {
   overflow: hidden;
   background: #FFFFFF;
-  /* 2 dp */
-
-  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 3px 4px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.14);
   border-radius: 4px;
-
   max-width: 250px;
   margin-top: 15px;
 }
--- src/components/CardCharacterList.vue
+++ src/components/CardCharacterList.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
+  <div class="q-pa-md row items-start q-gutter-md card-list">
     <CardCharacter
+        class="card-item"
         v-for="character in characters"
         :character="character"
     ></CardCharacter>
@@ -20,5 +21,17 @@ defineProps<CharacterList>()
 </script>
 
 <style scoped>
-
+.card-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.card-item {
+  margin: 40px;
+  transition: 0.5s;
+}
+.card-item:hover {
+  transform: scale(1.05);
+  transition: 0.3s;
+}
 </style>
