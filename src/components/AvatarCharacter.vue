da4c263c9b09b7f73a894de450710fed422e3598
Maycon Liborio <liborio.md@gmail.com>
2023-03-05 21:44:15 -0300

    :sparkles: feat: Componente AvatarCharacter

---
+++ src/components/AvatarCharacter.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="character-avatar-box">
+      <img class="character-avatar-image" :src="image"
+           :alt="`${name}, personagem da série Rick and Morty`">
+    </div>
+    <h2 class="character-name"> {{ name }} </h2>
+</template>
+
+<script setup lang="ts">
+interface AvatarCharacterProps {
+  image: string,
+  name: string
+}
+defineProps<AvatarCharacterProps>()
+</script>
+
+<style scoped>
+.character-avatar-box {
+  display: flex;
+  max-width: 300px;
+  max-height: 300px;
+  height: 75vw;
+  width: 75vw;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 5px solid #F2F2F7;
+}
+
+.character-name {
+  margin-top: 2%;
+  font-weight: 600;
+  font-size: 48px;
+  line-height: 56px;
+}
+
+@media screen and (max-width: 500px) {
+  .character-name {
+    font-size: 10vw;
+  }
+}
+
+</style>
