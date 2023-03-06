f6b75eba87cda4c4a5823ca7c604dd97097f1198
Maycon Liborio <liborio.md@gmail.com>
2023-03-05 23:25:59 -0300

    :sparkles: feat: Adicionando PortalLoading a CharacterList view

--- src/views/CharacterList.vue
+++ src/views/CharacterList.vue
@@ -5,6 +5,7 @@ import {computed, onMounted, ref} from "vue";
 import client from "../graphql/client";
 import CardCharacterList from "@/components/CardCharacterList.vue";
 import Button from "@/components/Button.vue";
+import LoadingPortal from "@/components/LoadingPortal.vue";
 
 const pagination = ref<Pagination>({next: 1, count: 0, pages: 1, current: 1})
 const characters = ref<Character[]>([])
@@ -18,7 +19,7 @@ onMounted(async () => {
   await getCharacters()
 })
 
-async function getCharacters(page: number = 1): Promise<void> {
+async function getCharacters(page: number = 1): never {
   loading.value = true;
 
   const variables = {
@@ -45,11 +46,17 @@ async function getCharacters(page: number = 1): Promise<void> {
     ...info,
     current: page,
   }
-  loading.value = false;
+
+  setTimeout(() => {
+    loading.value = false
+  }, 600)
 }
 </script>
 
 <template>
+  <LoadingPortal v-if="loading || !characters"></LoadingPortal>
+
+  <div id="character-list-page" class="suavization-animation" v-else>
     <img class="logo" src="/src/assets/image/logo.png" alt="Logo da série de televisão Rick and Morty">
 
     <div class="q-gutter-md" style="max-width: 70%">
@@ -60,12 +67,20 @@ async function getCharacters(page: number = 1): Promise<void> {
     <CardCharacterList :characters="characters"></CardCharacterList>
 
     <Button v-if="showMore" label="Show more" @click="getCharacters(pagination.next)"/>
+  </div>
 </template>
 
 <style>
 .logo {
-  margin-top: 100px;
-  margin-bottom: 100px;
-  max-width: 40%;
+  margin: 100px 0;
+  max-width: 70%;
 }
+
+#character-list-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
 </style>
