2601b2a2180cda878d3c9330b4e60ba965b0ec99
Maycon Liborio <liborio.md@gmail.com>
2023-03-06 15:04:54 -0300

    :package: build: Removendo vite-plugin-graphql

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import graphqlPlugin from 'vite-plugin-graphql';
 import {quasar, transformAssetUrls} from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
@@ -13,7 +12,6 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000000,
   },
   plugins: [
-      graphqlPlugin,
     vue({
       template: { transformAssetUrls }
     }),
