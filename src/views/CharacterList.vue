ff6d11d5c140aa270b7bb1fc6884f0817bdd3c21
Maycon Liborio <liborio.md@gmail.com>
2023-03-06 12:54:55 -0300

    :sparkles: feat: Finalização da página de listagem dos personagens

--- src/views/CharacterList.vue
+++ src/views/CharacterList.vue
@@ -6,20 +6,25 @@ import client from "../graphql/client";
 import CardCharacterList from "@/components/CardCharacterList.vue";
 import Button from "@/components/Button.vue";
 import LoadingPortal from "@/components/LoadingPortal.vue";
+import LoadingInfinite from "@/components/LoadingInfinite.vue";
 
-const pagination = ref<Pagination>({next: 1, count: 0, pages: 1, current: 1})
+const pagination = ref<Pagination>({next: 1, count: 0, pages: 1, current: 0})
 const characters = ref<Character[]>([])
 const nameFilter = ref<string>('')
-const loading = ref(false)
+const loading = ref(true)
+const filterLoading = ref(false)
+const infiniteScroll = ref(false)
+const lastFilter = ref(null)
+const showMoreLoading = computed(() => !loading.value && showMore.value && !infiniteScroll.value)
 const showMore = computed(() =>
-    pagination.value.current !== pagination.value.pages
+    pagination.value.current < pagination.value.pages
 )
 
 onMounted(async () => {
   await getCharacters()
 })
 
-async function getCharacters(page: number = 1): never {
+async function getCharacters(page: number = 1) {
   loading.value = true;
 
   const variables = {
@@ -27,60 +32,122 @@ async function getCharacters(page: number = 1): never {
     nameFilter: nameFilter.value
   }
 
-  const data: CharactersData = await client.request(GET_CHARACTERS_CARD_DATA, variables);
+  const timeoutPopulateData = () => {
+    loading.value = false
+    lastFilter.value = variables.nameFilter
+    pagination.value = {
+      ...info,
+      current: page,
+    }
+  }
+  const data: CharactersData = await client.request(GET_CHARACTERS_CARD_DATA, variables)
 
   const {info, results} = data.characters
 
   if (page === 1) {
-    characters.value = results;
+    setTimeout(() => {
+      characters.value = results;
+      timeoutPopulateData()
+    }, 600)
   }
 
   if (page > 1 && page === pagination.value.next) {
-    characters.value = [
-      ...characters.value,
-      ...results,
-    ]
+    setTimeout(() => {
+      characters.value = [
+        ...characters.value,
+        ...results,
+      ]
+      timeoutPopulateData()
+    }, 600)
   }
+}
 
-  pagination.value = {
-    ...info,
-    current: page,
+async function filterCharacters() {
+  if (lastFilter.value !== nameFilter.value) {
+    filterLoading.value = true;
+    await getCharacters();
+
+    setTimeout(() => {
+      filterLoading.value = false;
+    }, 600)
   }
+}
+
+async function showMoreCharacters() {
+  infiniteScroll.value = true
+
+  await getCharacters(pagination.value.next)
 
   setTimeout(() => {
-    loading.value = false
+    infiniteScroll.value = false
   }, 600)
 }
 </script>
 
 <template>
-  <LoadingPortal v-if="loading || !characters"></LoadingPortal>
-
-  <div id="character-list-page" class="suavization-animation" v-else>
+  <div id="character-list-page" class="suavization-animation">
     <img class="logo" src="/src/assets/image/logo.png" alt="Logo da série de televisão Rick and Morty">
 
-    <div class="q-gutter-md" style="max-width: 70%">
-      <q-input v-model="nameFilter" label="Filter by name..."/>
-      <Button label="Filter" @click="getCharacters()"></Button>
+    <div class="row filter-box">
+      <q-input
+          outlined
+          class="col-12"
+          v-model="nameFilter"
+          label="Filter character(s) by name..."
+          @keydown.tab="filterCharacters"
+          @keydown.enter="filterCharacters"
+          @blur="filterCharacters"
+      />
     </div>
 
-    <CardCharacterList :characters="characters"></CardCharacterList>
-
-    <Button v-if="showMore" label="Show more" @click="getCharacters(pagination.next)"/>
+    <LoadingPortal v-if="loading && !infiniteScroll" class="suavization-animation"></LoadingPortal>
+    <CardCharacterList v-else class="suavization-animation" :characters="characters"></CardCharacterList>
+    <div v-if="characters.length && characters.length > 0" style="display: flex; width: 100%; justify-content: flex-end; font-size: 14px; letter-spacing: 1.5px">
+      <span>
+        Showing: <strong>{{ characters.length }}</strong> of <strong> {{ pagination.count }}</strong>
+      </span>
+    </div>
+    <LoadingInfinite v-if="infiniteScroll" class="suavization-animation" style="margin-top: 10px;"/>
+    <Button
+        v-if="showMoreLoading && !infiniteScroll"
+        class="suavization-animation"
+        style="margin-top: 10px"
+        label="Show more"
+        @click="showMoreCharacters()"
+    />
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .logo {
-  margin: 100px 0;
-  max-width: 70%;
+  width: 100%;
+  max-width: 500px;
+  margin: 20px 0 60px;
 }
 
 #character-list-page {
   display: flex;
+  width: 100%;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  padding: 0;
 }
 
+.filter-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 0;
+  margin-bottom: 60px;
+}
+
+.q-field--outlined .q-field__control {
+  border-radius: 8px !important;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  color: rgba(0, 0, 0, 0.5);
+}
 </style>
